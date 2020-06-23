960066fb9d4ce9792ba7a1013631c7d47bbb44a1
duzzy1202 <duzzy1202@gmail.com>
2020-06-23 13:04:58 +0900

    작업 2

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 function MobileSideBar__init() {
     var $btnToggleMobileSideBar = $('.btn-toggle-mobile-side-bar');
+    var $btnToggleMoblieSideBarMenu = $('.mobile-side-bar > .menu-box > ul > li');
 
     $btnToggleMobileSideBar.click(function() {
-        if ($(this).hasClass('active')) {
+        if ( $(this).hasClass('active') ) {
+            $(this).removeClass('active');
+            $('.mobile-side-bar').removeClass('active');
+        }
+        else {
+            $(this).addClass('active');
+            $('.mobile-side-bar').addClass('active');
+        }
+    });
+    
+    $btnToggleMoblieSideBarMenu.click(function() {
+        if ( $(this).hasClass('active') ) {
             $(this).removeClass('active');
         }
         else {
